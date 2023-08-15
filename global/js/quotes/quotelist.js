e30baf0e871e0c29b3b4394fb147916a1066bad4
techno-sorcery <inquisitivecalculator@gmail.com>
2023-08-15 03:10:56 -0700

    Jquery SUX

--- global/js/quotes/quotelist.js
+++
@@ -1,8 +0,0 @@
-const quotes = [
-	"\"Man is still the most extraordinary computer of all.\" -JFK",
-	"\"Life in two-dimensions is a mass-production scheme.\" -Rush",
-	"\"Will they read of us with sadness, for the seeds that we let grow?\" -Rush",
-	"\"We watch in reverence, as narcissus is turned to a flower.\" -Genesis",
-	"\"For the freedom of life everlasting!\" -Yes",
-	"\"Machines are living too, working for me and you.\" -OMD"
-]
